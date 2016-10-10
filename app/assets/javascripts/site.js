9dbe09a9b191cb07026f3b0ddb7c082d0497c28f
Andres Howard <ahowardm@gmail.com>
2016-10-10 20:53:46 -0300

    6.10 AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,15 @@
-$(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+var refreshRating = function(){
+	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets/images',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
       }
     });
+}
+
+$(document).on('ready page:load ajaxSuccess', function(){
+    refreshRating();
+
     $('.img-zoom').elevateZoom();
 });
