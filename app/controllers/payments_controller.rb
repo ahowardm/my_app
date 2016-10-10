8936280e2880310c00d3bde2329e030cb3aad5f5
Andres Howard <ahowardm@gmail.com>
2016-10-10 01:52:29 -0300

    6.6 Creating Payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,7 @@ class PaymentsController < ApplicationController
       )
     
     if charge.paid
-      Order.create(@product, @user, @product.price)
+      Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
     end
 
     rescue Stripe::CardError => e
