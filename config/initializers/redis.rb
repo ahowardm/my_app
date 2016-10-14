5dd12865a34ce7d10132d9aab545e3fbf9e9ccfe
Andres Howard <ahowardm@gmail.com>
2016-10-14 10:36:45 -0300

    5.11 Fixes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   $redis = Redis.new(url: ENV["REDIS_URL"])
-elseif Rails.env.development?
+elsif Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
